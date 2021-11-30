e814db2ce0a80906fd83570ee3ae2517d182dee8
_.Jeydi <kadiongoe@yahoo.fr>
2021-11-30 08:24:03 +0100

    user added

--- src/router/views/users/index.vue
+++ src/router/views/users/index.vue
@@ -1,7 +1,45 @@
 <template>
 	<Layout>
 		<PageHeader :title="title" :items="items" />
+		<b-row v-if="showList" align-h="center">
+			<b-col md="8">
+				<div class="card">
+					<div class="card-body">
+						<b-row class="mb-2">
+							<b-col>
+								<h4 class="card-title mb-4">Liste des utilisateurs</h4>
+							</b-col>
 
+						</b-row>
+						<div class="table-responsive mb-0">
+							<table class="table table-centered table-nowrap">
+								<thead class="thead-light">
+									<tr>
+										<th style="width: 20px;">
+											#
+										</th>
+										<th>Nom</th>
+										<th>Actif</th>
+										<th>Creation</th>
+										<th>Téléphone</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr v-for="data in users" :key="data.id">
+										<td>{{data.id}}</td>
+										<td>{{data.name}}</td>
+										<td>{{data.disabled}}</td>
+										<td>{{data.creation}}</td>
+										<td>{{data.telephone}}</td>
+									</tr>
+								</tbody>
+							</table>
+
+						</div>
+					</div>
+				</div>
+			</b-col>
+		</b-row>
 	</Layout>
 </template>
 
@@ -9,21 +47,32 @@
 	import Layout from "../../layouts/main";
 	import appConfig from "@/app.config";
 	import PageHeader from "@/components/page-header";
-	import { mapActions, mapState } from "vuex";
+	import { mapActions, mapState, mapGetters } from "vuex";
 
 	export default {
 		name: "users",
 		page: {
-			title: "Chaine",
+			title: "Users",
 			meta: [{ name: "description", content: appConfig.description }],
 		},
 		components: {
 			Layout,
 			PageHeader,
 		},
+		computed: {
+			// ...mapState({
+			// 	optionsCategories: (state) => state.categorie.optionsCategories,
+			// 	channels: (state) => state.categorie.channels,
+			// }),
+			...mapGetters("categorie",{
+				users: "getUsers"
+			}),
+		},
 		data() {
 			return {
 				title: "Users",
+				showList: true,
+				showModal: true,
 				items: [
 					{
 						text: "Users",
--- src/state/modules/categorie.js
+++ src/state/modules/categorie.js
@@ -11,6 +11,7 @@ const state = {
   checkCat: { lastDocCat: null, documentLimit: 2, hasMore: true },
   checkColor: { lastDocColor: null, documentLimit: 2, hasMore: true },
   checkChannel: { lastDocChannel: null, documentLimit: 2, hasMore: true },
+  checkUser: { lastDocChannel: null, documentLimit: 2, hasMore: true },
 
   colors: [],
   channels: [],
@@ -35,11 +36,17 @@ const mutations = {
   ADD_COLOR(state, data) {
     state.colors.unshift(data);
   },
-  SET_LASTDOC_CAT(state, data) {
-    state.lastDocCat = data;
+  ADD_CHANNEL(state, data) {
+    state.channels.unshift(data);
+  },
+  ADD_USER(state, data) {
+    state.users.unshift(data);
   },
   HAS_MORE_COLOR(state, e) {
     state.checkColor.hasMore = !!e;
+  },
+  HAS_MORE_USER(state, e) {
+    state.checkUser.hasMore = !!e;
   },
   HAS_MORE_CHANNEL(state, e) {
     state.checkChannel.hasMore = !!e;
@@ -50,12 +57,16 @@ const mutations = {
   SET_LASTDOC_CHANNEL(state, data) {
     state.lastDocChannel = data;
   },
+  SET_LASTDOC_CAT(state, data) {
+    state.lastDocCat = data;
+  },
   SET_LASTDOC_COLOR(state, data) {
     state.lastDocColor = data;
   },
-  ADD_CHANNEL(state, data) {
-    state.channels.unshift(data);
+  SET_LASTDOC_USER(state, data) {
+    state.lastDocUser = data;
   },
+
   ADD_OPTION_CAT(state, category) {
     state.optionsCategories.unshift({
       value: category.nom,
@@ -69,9 +80,12 @@ const mutations = {
   }
 };
 const actions = {
-  init({ commit, dispatch, rootState }) {
+  async init({ commit, dispatch, rootState, state }) {
     dispatch("listenCat");
     commit("INIT_DONE", true);
+    if (state.users.length == 0) {
+      await dispatch("getUsers");
+    }
   },
   listenCat({ commit }, pay) {
     firestore.collection("categories").onSnapshot(
@@ -246,12 +260,55 @@ const actions = {
     } else {
       console.log("Plus aucun color a afficher");
     }
+  },
+  async getUsers({ commit, dispatch }, last) {
+    var { documentLimit, hasMore, lastDocUsers } = state.checkColor;
+    var snap = null;
+    if (hasMore) {
+      if (lastDocUsers == null) {
+        snap = await firestore
+          .collection("utilisateurs")
+          .orderBy("nom")
+          .limit(documentLimit)
+          .get();
+      } else {
+        snap = await firestore
+          .collection("utilisateurs")
+          .orderBy("nom")
+          .startAfter(lastDocUsers)
+          .limit(documentLimit)
+          .get();
+      }
+      commit("SET_LASTDOC_USER", snap.docs[snap.docs.length - 1]);
+      if (snap.docs.length < documentLimit) {
+        console.log("now there is no more user");
+        commit("HAS_MORE_USER", false);
+      }
+      for (var doc of snap.docs) {
+        console.log(snap);
+        commit("ADD_USER", { id: doc["id"], ...doc.data() });
+      }
+    } else {
+      console.log("Plus aucun utilistaeur à afficher");
+    }
   }
 };
 
 const getters = {
   currentOrderData: (state, getters) => {
     return [];
+  },
+  getUsers: (state, getters) => {
+    return state.users;
+  },
+  getColors: (state, getters) => {
+    return state.colors;
+  },
+  getChannels: (state, getters) => {
+    return state.channels;
+  },
+  getCategorys: (state, getters) => {
+    return state.categorys;
   }
 };
 
